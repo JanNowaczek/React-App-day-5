3dc1f2794c7308a33dd7f426ecb3c3650238fb6a
mrozo2m <mrozo2m@interia.pl>
2019-04-05 14:00:19 +0200

    styles added to UserItem

--- src/UserItem.js
+++ src/UserItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
+const styles = {
+    main: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between'
+    }
+}
 
 const UserItem = (props) => (
-    <div>
-        {props.user.name}
+    <div
+        style={styles.main}
+    >
+        <div>
+            {props.user.avatar}
+        </div>
+        <div>
+            {props.user.name}
+            {props.user.email}
+        </div>
+        <div>
+            <button>X</button>
+        </div>
     </div>
 )
 
