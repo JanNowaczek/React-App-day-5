68ddfc90b7e441c0300febdae001a12ac0ec0fdd
mrozo2m <mrozo2m@interia.pl>
2019-04-05 14:10:24 +0200

    avatars added to UserItem

--- src/UserItem.js
+++ src/UserItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import userPlaceholder from './user.svg'
+
 const styles = {
     main: {
         width: '100%',
         display: 'flex',
         justifyContent: 'space-between'
+    },
+    img: {
+        width: 30,
+        heigh: 30,
+        borderRadius: '50%',
     }
 }
 
@@ -13,6 +20,11 @@ const UserItem = (props) => (
         style={styles.main}
     >
         <div>
+            <img
+                style={styles.img}
+                src={props.user.avatar || userPlaceholder}
+                alt={props.user.name}
+            />
             {props.user.avatar}
         </div>
         <div>
