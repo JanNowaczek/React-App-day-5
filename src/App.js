c5ef9ad9ce4a301165f23a3464d9faa92ac2e692
mrozo2m <mrozo2m@interia.pl>
2019-04-05 13:53:01 +0200

    UserItem added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import './logic'
 import AddUser from './AddUser'
+import ListUsers from './ListUsers'
 
 const App = (props) => (
   <div>
+      <ListUsers />
       <AddUser />
   </div>
 )
