24d112815a50a5b708770576736c8decadb9f2d1
mrozo2m <mrozo2m@interia.pl>
2019-04-05 09:36:34 +0200

    style css added to buttons

--- src/ButtonGreen.js
+++ src/ButtonGreen.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './ButtonGreen.css'
 
 const ButtonGreen = (props) => (
       <div
--- src/ButtonYellow.js
+++ src/ButtonYellow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ButtonYellow = (props) => (
       <div
